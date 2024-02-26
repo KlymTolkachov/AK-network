ede5708787958b1897b290754464b1406580862d
klimikand <klimikand@gmail.com>
2024-02-26 17:53:40 +0200

    finish auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {AuthModule} from './auth/auth.module';
+import {ConfigModule} from "@nestjs/config";
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
-    imports: [AuthModule],
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot('mongodb://localhost/AK', {
+            authSource: 'MONGO_AUTH_SOURCE=admin',
+        }),
+        AuthModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,33 @@
 import {Module} from '@nestjs/common';
 import {AuthController} from './auth.controller';
 import {AuthService} from './auth.service';
+import {MongooseModule} from "@nestjs/mongoose";
+import {UserModel, UserSchema} from "./user.model";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {JwtModule} from "@nestjs/jwt";
+import {getJWTConfig} from "../configs/jwt.config";
+import {PassportModule} from "@nestjs/passport";
+import {JwtStrategy} from "./strategies/jwt.strategy";
 
 @Module({
     controllers: [AuthController],
-    providers: [AuthService]
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: UserModel.name,
+                schema: UserSchema,
+                collection: 'Users'
+            },
+        ]),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: getJWTConfig,
+        }),
+        ConfigModule,
+        PassportModule,
+    ],
+    providers: [AuthService, JwtStrategy]
 })
 export class AuthModule {
 }

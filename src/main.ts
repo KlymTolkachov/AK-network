cd856506086c29226de74aeaabbd74edf23fe184
klimikand <klimikand@gmail.com>
2024-03-12 13:42:39 +0200

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix('api')
+    app.setGlobalPrefix('api');
+    app.enableCors();
 
     const config = new DocumentBuilder()
         .setTitle('AK Network')
